64061027089f50b065232ffb3d0c4c1eef8d0ca5
FoozyFoo <austin.lavaughn@gmail.com>
2023-06-26 20:24:46 +0000

    actual fix

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,9 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.4"
+module "network" {
+    source  = "app.terraform.io/austin-test-organization3/network/google"
+    version = "3.4.0"
 
     project_id   = var.project
     network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
 
     subnets = [
         {
