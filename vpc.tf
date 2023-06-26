cb1efff7be360f2f8caddb666c47f19fe6e882e5
FoozyFoo <austin.lavaughn@gmail.com>
2023-06-26 20:18:10 +0000

    another attempted fix

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+    version = "~> 3.4"
 
     project_id   = var.project
     network_name = "gaurav-network"
@@ -12,23 +12,5 @@ module "vpc" {
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
-    ]
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-        {
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
     ]
 }
